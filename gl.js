a5edcc55cbbe2954eedbe1e94e91afa80d675201
Waiel Aljnabi <sadmeat@hotmail.com>
2021-12-27 10:28:18 +0100

    Reworked graphics performance and UI

--- gl.js
+++ gl.js
@@ -1,27 +1,26 @@
-var time = 0.0;
-var mic;
-var mouseX = 0;
-var mouseY = 0;
-var mousePressed = false;
+let FPS = 30;
 
+let time = 0.0;
+let mic;
+let mouseX = 0;
+let mouseY = 0;
+let mousePressed = false;
 
-  //if(isExe()) document.querySelector('#goto').remove();
-  
 function glmain() {
   const canvas = document.querySelector('#canvas');
-  const gl = canvas.getContext('webgl', {antialias: true});
-  
+  const gl = canvas.getContext('webgl', { antialias: true });
+
   canvas.onmousemove = mouseHandle;
   canvas.onmousedown = mouseHandle;
   canvas.onmouseup = mouseHandle;
-  
+
   window.addEventListener('resize', resizeCanvas, false);
   function resizeCanvas() {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight-50;
+    canvas.height = window.innerHeight;
   }
   resizeCanvas();
-  
+
   const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
   const programInfo = {
@@ -55,15 +54,19 @@ function glmain() {
   const texture4 = loadTexture(gl, 'tex_fabric.png');
 
   console.log(programInfo, buffers, gl)
-  
-  
-  var then = 0;
-  function render(now) {
-    now *= 0.001;  // convert to seconds
-    const deltaTime = now - then;
-    then = now;
 
-    drawScene(gl, programInfo, buffers, [texture1, texture2, texture3, texture4], deltaTime);
+
+  let then = 0;
+  function render(now) {
+    const deltaTime = (now - then) / 1000;
+    if (deltaTime > 1 / FPS) {
+      then = now;
+      drawScene(gl, programInfo, buffers, [texture1, texture2, texture3, texture4], deltaTime);
+      // console.log("noDrop");
+    } else {
+      // console.log("Drop");
+    }
+
 
     requestAnimationFrame(render);
   }
@@ -99,11 +102,11 @@ function initBuffers(gl) {
   const indexBuffer1 = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer1);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(v.t1), gl.STATIC_DRAW);
-  
+
   const indexBuffer2 = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer2);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(v.t2), gl.STATIC_DRAW);
-  
+
   const indexBuffer3 = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer3);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(v.t3), gl.STATIC_DRAW);
@@ -127,10 +130,10 @@ function loadTexture(gl, url) {
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 1, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255]));
 
   const image = new Image();
-  image.onload = function() {
+  image.onload = function () {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);        
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   };
   image.src = url;
 
@@ -138,157 +141,235 @@ function loadTexture(gl, url) {
 }
 
 
-let lid=0;
+let lid = 0;
 let headX = 0;
 let headY = 0;
 
-function calcBoneMatrix() {
-  if(mousePressed) {
-     var a = 0.5;
-     headX = headX * a + (1-a) * mouseX; 
-     headY = headY * a + (1-a) * mouseY; 
-  }
-  
-  var vol = (mic?mic.getLevel(0.7):0);
-  var sens = 1-document.getElementById("sensitivity").value;
-  var vol2 = Math.pow(Math.max(vol-0.05, 0), 1.5*sens);
-  
-  var t = 0.01*Math.sin(time*1.5);
-  
-  var mNeck1 = mat4.create();
-  var mNeck2 = mat4.create();
-  var mNeck3 = mat4.create();
-  var mHead = mat4.create();
-  var mTopLip = mat4.create();
-  var mBottomLip = mat4.create();
-  var mEyeLid_R = mat4.create();
-  var mEyeLid_L = mat4.create();
-  
-  mat4.set(mNeck3, ...bonesInv[2]);
-  mat4.rotate(mNeck3, mNeck3, 0.5*(headX), [0, 0, 1])
-  mat4.mul(mNeck3, bonesMat[2], mNeck3);
-  
-  mat4.invert(mHead, ...bonesLocal[3]);
-  mat4.rotate(mHead, mHead, 0.5*(headY), [1, 0, 0])
-  mat4.rotate(mHead, mHead, 0.5*(headX), [0, 0, 1])
-  mat4.mul(mHead,  mNeck3, mHead)
-  
-  mat4.invert(mTopLip, ...bonesLocal[4]);
-  //mat4.rotate(mTopLip, mTopLip, 0.1*vol2+t, [-1, 0, 0])
-  mat4.translate(mTopLip, mTopLip, [0, 0.1*vol2, 0.5*vol2+t])
-  mat4.mul(mTopLip, mHead, mTopLip);
-  
-  mat4.invert(mBottomLip, ...bonesLocal[5]);
-  //mat4.rotate(mBottomLip, mBottomLip, 1.1*vol2+t, [0, 0, 1])
-  mat4.translate(mBottomLip, mBottomLip, [0, 0.1*vol2, -0.5*vol2-t])
-  mat4.mul(mBottomLip, mHead, mBottomLip);
-  
-  var x = (time%7 < 0.25 && document.querySelector("#activateBlink").checked) ? Math.sin(time%1*4*Math.PI):0;
+const _Neck1 = mat4.create();
+const mNeck1 = mat4.create();
+const _Neck2 = mat4.create();
+const mNeck2 = mat4.create();
+const _Neck3 = mat4.create();
+const mNeck3 = mat4.create();
+const _Head = mat4.create();
+const mHead = mat4.create();
+const _TopLip = mat4.create();
+const mTopLip = mat4.create();
+const _BottomLip = mat4.create();
+const mBottomLip = mat4.create();
+const _EyeLid_R = mat4.create();
+const mEyeLid_R = mat4.create();
+const _EyeLid_L = mat4.create();
+const mEyeLid_L = mat4.create();
 
+let activeBlinkCheckbox;
+let sensitivitySlider;
+let boostInputSlider;
+let boostOutputSlider;
+let sensitivityLabel;
+let boostInputLabel;
+let boostOutputLabel;
+let fpsSlider;
+let fpsLabel;
+
+function precalcBoneMatrix() {
+  activeBlinkCheckbox = document.querySelector("#activateBlink");
+  sensitivitySlider = document.getElementById("sensitivity");
+  boostInputSlider = document.getElementById("boostInput");
+  boostOutputSlider = document.getElementById("boostOutput");
+  sensitivityLabel = document.getElementById("sensitivityLabel");
+  boostInputLabel = document.getElementById("boostInputLabel");
+  boostOutputLabel = document.getElementById("boostOutputLabel");
+  sensitivitySlider.value = parseFloat(localStorage.getItem("sensitivity") || 0.5);
+  boostInputSlider.value = parseFloat(localStorage.getItem("boostInput") || 2.0);
+  boostOutputSlider.value = parseFloat(localStorage.getItem("boostOutput") || 2.0);
+  sensitivityLabel.innerText = parseFloat(sensitivitySlider.value).toFixed(2);
+  boostInputLabel.innerText = parseFloat(boostInputSlider.value).toFixed(2);
+  boostOutputLabel.innerText = parseFloat(boostOutputSlider.value).toFixed(2);
+  sensitivitySlider.onchange = () => localStorage.setItem("sensitivity", sensitivitySlider.value);
+  sensitivitySlider.oninput = () => sensitivityLabel.innerText = parseFloat(sensitivitySlider.value).toFixed(2);
+  boostInputSlider.onchange = () => localStorage.setItem("boostInput", boostInputSlider.value);
+  boostInputSlider.oninput = () => boostInputLabel.innerText = parseFloat(boostInputSlider.value).toFixed(2);
+  boostOutputSlider.onchange = () => localStorage.setItem("boostOutput", boostOutputSlider.value);
+  boostOutputSlider.oninput = () => boostOutputLabel.innerText = parseFloat(boostOutputSlider.value).toFixed(2);
   
-  mat4.invert(mEyeLid_R, ...bonesLocal[6]);
-  mat4.translate(mEyeLid_R, mEyeLid_R, [0.1*x*x, 0, -0.05*x*x])
-  mat4.mul(mEyeLid_R, mHead, mEyeLid_R);
-  
-  mat4.invert(mEyeLid_L, ...bonesLocal[7]);
-  mat4.translate(mEyeLid_L, mEyeLid_L, [-0.1*x*x, 0, -0.05*x*x])
-  mat4.mul(mEyeLid_L, mHead, mEyeLid_L);
-  
-  return [...mNeck1, ...mNeck2, ...mNeck3, ...mHead, 
-          ...mTopLip, ...mBottomLip, ...mEyeLid_R, ...mEyeLid_L]
-    
+  fpsSlider = document.getElementById("framerate");
+  fpsLabel = document.getElementById("framerateLabel");
+  fpsSlider.value = parseFloat(localStorage.getItem("framerate") || 30);
+  fpsLabel.innerText = fpsSlider.value;
+  FPS = parseFloat(fpsSlider.value);
+  fpsSlider.onchange = () => {
+    localStorage.setItem("framerate", fpsSlider.value);
+  }
+  fpsSlider.oninput = () => {
+    fpsLabel.innerText = fpsSlider.value;
+    FPS = parseFloat(fpsSlider.value);
+  }
+  mat4.set(_Neck3, ...bonesInv[2]);
+  mat4.invert(_Head, ...bonesLocal[3]);
+  mat4.invert(_TopLip, ...bonesLocal[4]);
+  mat4.invert(_BottomLip, ...bonesLocal[5]);
+  mat4.invert(_EyeLid_R, ...bonesLocal[6]);
+  mat4.invert(_EyeLid_L, ...bonesLocal[7]);
 }
 
-function drawScene(gl, programInfo, buffers, texture, deltaTime) {
-  var color = document.querySelector('#bcolor').value.match(/[\dA-Fa-f]{2}/g);
-  if(color!==null && color.length===3) {
-      color = color.map((v)=>parseInt(v, 16)/255);
-      gl.clearColor(color[0], color[1], color[2], 1.0);
-  } else {
-      gl.clearColor(0.0, 1.0, 0.0, 1.0);
-  }
-  gl.clearDepth(1.0);
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL);
 
+function calcBoneMatrix() {
+  if (mousePressed) {
+    let a = 0.5;
+    headX = headX * a + (1 - a) * mouseX * 4;
+    headY = headY * a + (1 - a) * mouseY * 1;
+  }
+
+  let sensitivity = 1 - Math.sqrt(sensitivitySlider.value);
+  let boostIn = boostInputSlider.value;
+  let boostOut = boostOutputSlider.value;
+
+  let level = mic ? mic.getLevel() : 0;
+  let levelAdj = (level - 0.05) / 0.95;
+  let input = Math.max(levelAdj, 0);
+  let inputBoosted = Math.min(boostIn * input, 1);
+  let output = Math.pow(inputBoosted, 4 * sensitivity) / 0.95;
+  let outputBoosted = boostOut * output;
+  let volume = outputBoosted;
+
+  // console.log(
+  //   input.toFixed(3),
+  //   inputBoosted.toFixed(3),
+  //   output.toFixed(3),
+  //   outputBoosted.toFixed(3),
+  // );
+
+  let t = 0.01 * Math.sin(time * 1.5);
+
+  // mat4.copy(mNeck1, _Neck1);
+  // mat4.copy(mNeck2, _Neck2);
+
+  mat4.copy(mNeck3, _Neck3);
+  mat4.rotateZ(mNeck3, mNeck3, 0.5 * (headX))
+  mat4.mul(mNeck3, bonesMat[2], mNeck3);
+
+  mat4.copy(mHead, _Head);
+  mat4.rotateX(mHead, mHead, 0.5 * (headY))
+  mat4.rotateZ(mHead, mHead, 0.5 * (headX))
+  mat4.mul(mHead, mNeck3, mHead)
+
+  mat4.copy(mTopLip, _TopLip);
+  //mat4.rotate(mTopLip, mTopLip, 0.1*volume+t, [-1, 0, 0])
+  mat4.translate(mTopLip, mTopLip, [0, 0.1 * volume, 0.5 * volume + t])
+  mat4.mul(mTopLip, mHead, mTopLip);
+
+  mat4.copy(mBottomLip, _BottomLip);
+  //mat4.rotate(mBottomLip, mBottomLip, 1.1*volume+t, [0, 0, 1])
+  mat4.translate(mBottomLip, mBottomLip, [0, 0.1 * volume, -0.5 * volume - t])
+  mat4.mul(mBottomLip, mHead, mBottomLip);
+
+  let x = (time % 7 < 0.25 && activeBlinkCheckbox.checked) ? Math.sin(time % 1 * 4 * Math.PI) : 0;
+
+  mat4.copy(mEyeLid_R, _EyeLid_R);
+  mat4.translate(mEyeLid_R, mEyeLid_R, [0.1 * x * x, 0, -0.05 * x * x])
+  mat4.mul(mEyeLid_R, mHead, mEyeLid_R);
+
+  mat4.copy(mEyeLid_L, _EyeLid_L);
+  mat4.translate(mEyeLid_L, mEyeLid_L, [-0.1 * x * x, 0, -0.05 * x * x])
+  mat4.mul(mEyeLid_L, mHead, mEyeLid_L);
+
+  return [...mNeck1, ...mNeck2, ...mNeck3, ...mHead,
+  ...mTopLip, ...mBottomLip, ...mEyeLid_R, ...mEyeLid_L];
+}
+
+
+const projectionMatrix = mat4.create();
+const modelViewMatrix = mat4.create();
+let sceneOnce = true;
+
+function drawScene(gl, programInfo, buffers, texture, deltaTime) {
+  if (sceneOnce) {
+    gl.clearColor(0.0, 1.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+  }
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
 
   const fieldOfView = 50 * Math.PI / 180;
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
-  const projectionMatrix = mat4.create();
 
   mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
-  const modelViewMatrix = mat4.create();
-  
-  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, -4.0, -6]); 
-  //mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, -4.0, -20.5]); 
-  mat4.rotate(modelViewMatrix, modelViewMatrix, -Math.PI/2+0.01*Math.sin(time*1.5), [1, 0, 0]);
-  mat4.rotate(modelViewMatrix, modelViewMatrix, +0.125+0.06*Math.sin(time*0.6), [0, 0, 1]);
-  
-  {
+  mat4.fromTranslation(modelViewMatrix, [-0.0, -4.0, -6]);
+  //mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, -4.0, -20.5]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, -Math.PI / 2 + 0.01 * Math.sin(time * 1.5), [1, 0, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, +0.125 + 0.06 * Math.sin(time * 0.6), [0, 0, 1]);
+
+  if (sceneOnce) {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
     gl.vertexAttribPointer(programInfo.attribLocations.textureCoord, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
     gl.vertexAttribPointer(programInfo.attribLocations.normal, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.normal);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.tangent);
     gl.vertexAttribPointer(programInfo.attribLocations.tangent, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.tangent);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.boneIndex);
     gl.vertexAttribPointer(programInfo.attribLocations.boneIndex, 4, gl.UNSIGNED_BYTE, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.boneIndex);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.boneWeight);
     gl.vertexAttribPointer(programInfo.attribLocations.boneWeight, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.boneWeight);
   }
 
-  gl.useProgram(programInfo.program);
-  
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, texture[0]);
-  gl.activeTexture(gl.TEXTURE1);
-  gl.bindTexture(gl.TEXTURE_2D, texture[1]);
-  gl.activeTexture(gl.TEXTURE2);
-  gl.bindTexture(gl.TEXTURE_2D, texture[2]);
-  gl.activeTexture(gl.TEXTURE3);
-  gl.bindTexture(gl.TEXTURE_2D, texture[3]);
-  
+  if (sceneOnce)
+    gl.useProgram(programInfo.program);
+
+  if (sceneOnce) {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture[0]);
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, texture[1]);
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, texture[2]);
+    gl.activeTexture(gl.TEXTURE3);
+    gl.bindTexture(gl.TEXTURE_2D, texture[3]);
+  }
+
   gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
   gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
   gl.uniform1i(programInfo.uniformLocations.aoTexture, 0);
   gl.uniform1i(programInfo.uniformLocations.norTexture, 1);
   gl.uniform1i(programInfo.uniformLocations.hatTexture, 2);
   gl.uniform1i(programInfo.uniformLocations.fabricTexture, 3);
-  
+
+  if (sceneOnce)
+    precalcBoneMatrix();
+
   gl.uniformMatrix4fv(programInfo.uniformLocations.bones, false, calcBoneMatrix());
 
   {
     gl.uniform1i(programInfo.uniformLocations.eyes, false);
     gl.uniform1i(programInfo.uniformLocations.hat, true);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices3);
-    
-    if(document.querySelector("#activateHat").checked)
-        gl.drawElements(gl.TRIANGLES, v.t3.length, gl.UNSIGNED_SHORT, 0);
-    
+
+    if (document.querySelector("#activateHat").checked)
+      gl.drawElements(gl.TRIANGLES, v.t3.length, gl.UNSIGNED_SHORT, 0);
+
     gl.uniform1i(programInfo.uniformLocations.hat, false);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices1);
     gl.drawElements(gl.TRIANGLES, v.t1.length, gl.UNSIGNED_SHORT, 0);
-    
+
     gl.uniform1i(programInfo.uniformLocations.eyes, true);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices2);
     gl.drawElements(gl.TRIANGLES, v.t2.length, gl.UNSIGNED_SHORT, 0);
@@ -296,6 +377,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 
 
   time += deltaTime;
+  sceneOnce = false;
 }
 
 function initShaderProgram(gl, vsSource, fsSource) {
@@ -312,7 +394,7 @@ function initShaderProgram(gl, vsSource, fsSource) {
 
     return shader;
   }
-  
+
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
@@ -325,18 +407,15 @@ function initShaderProgram(gl, vsSource, fsSource) {
     alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
     return null;
   }
-  
+
   return shaderProgram;
 }
 
-function isExe() {
-    return new URL(window.location.href).searchParams.get("exe")!==null;
-}
 
-function mouseHandle(e){
-    mouseX = e.clientX/window.innerWidth*2 - 1;
-    mouseY = e.clientY/(window.innerHeight-50)*2 - 1;
-    mousePressed = e.buttons !==0;
+function mouseHandle(e) {
+  mouseX = e.clientX / window.innerWidth * 2 - 1;
+  mouseY = e.clientY / (window.innerHeight - 50) * 2 - 1;
+  mousePressed = e.buttons !== 0;
 }
 
 function toggleMic() {
